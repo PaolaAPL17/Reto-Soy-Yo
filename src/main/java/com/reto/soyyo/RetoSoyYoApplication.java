ee2fb10ed2a2f9ff00a9e97c3e5edc6f0129b3ae
paolap <paopin2024@gmail.com>
2025-08-26 17:14:15 +0200

    first commit

---
+++ src/main/java/com/reto/soyyo/RetoSoyYoApplication.java
@@ -0,0 +1,13 @@
+package com.reto.soyyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoSoyYoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoSoyYoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/soyyo/RetoSoyYoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.soyyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoSoyYoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
