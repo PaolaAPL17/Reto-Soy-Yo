c271164e10ed054c1dcb4a64b9dec745232e8695
paolap <paopin2024@gmail.com>
2025-09-02 00:42:03 +0200

    feat(user): add UserRequest

--- src/main/java/com/reto/soyyo/dtos/user/UserRequest.java
+++ src/main/java/com/reto/soyyo/dtos/user/UserRequest.java
@@ -1,4 +1,25 @@
 package com.reto.soyyo.dtos.user;
 
-public record UserRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank(message = "The email is required")
+    @Email(message = "Please provide a valid email address")
+    private String email;
+
+    @NotBlank(message = "The password is required")
+    @Size(min = 8, max = 128, message = "The password must be at least 8 characters long")
+    private String password;
 }
