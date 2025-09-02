6321b2c79a432ea25a4e6a687e11a8e5eb882909
paolap <paopin2024@gmail.com>
2025-09-03 01:15:31 +0200

    feat(auth): add JJWT dependencies for token generation and validation

---
+++ src/main/java/com/reto/soyyo/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,15 @@
+package com.reto.soyyo.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+
+public class JwtTokenProvider {
+}
