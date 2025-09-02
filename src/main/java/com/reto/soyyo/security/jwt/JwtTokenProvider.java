e460b42900b861f0568cbf37c96885107c9143d5
paolap <paopin2024@gmail.com>
2025-09-03 01:30:59 +0200

    feat(security): add JwtTokenProvider for JWT creation and validation

--- src/main/java/com/reto/soyyo/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/reto/soyyo/security/jwt/JwtTokenProvider.java
@@ -8,8 +8,45 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.security.Signature;
 import java.util.Date;
 
-
+@Component
 public class JwtTokenProvider {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    private final long EXPIRATION_TIME = 86400000;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningkey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            return  false;
+        }
+    }
 }
