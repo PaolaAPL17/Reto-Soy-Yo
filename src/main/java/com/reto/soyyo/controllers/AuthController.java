22ebf07ddfddd0eb1616cc207767c88fba4116ed
paolap <paopin2024@gmail.com>
2025-09-03 03:08:12 +0200

    fix(security): update jjwt dependencies to fix parserBuilder() error

--- src/main/java/com/reto/soyyo/controllers/AuthController.java
+++ src/main/java/com/reto/soyyo/controllers/AuthController.java
@@ -3,12 +3,13 @@ package com.reto.soyyo.controllers;
 import com.reto.soyyo.dtos.auth.LoginRequestDto;
 import com.reto.soyyo.dtos.auth.LoginResponseDto;
 import com.reto.soyyo.dtos.user.UserRequest;
-import com.reto.soyyo.models.User;
+import com.reto.soyyo.dtos.user.UserResponse;
 import com.reto.soyyo.services.AuthService;
 import com.reto.soyyo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +24,13 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody UserRequest userRequest) {
-        User registeredUser = userService.createUser(userRequest);
+    public ResponseEntity<UserResponse> registerUser(@Validated @RequestBody UserRequest userRequest) {
+        UserResponse registeredUser = userService.createUser(userRequest);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> loginUser(@RequestBody LoginRequestDto loginRequestDto) {
+    public ResponseEntity<LoginResponseDto> loginUser(@Validated @RequestBody LoginRequestDto loginRequestDto) {
         LoginResponseDto response = authService.authenticateUser(loginRequestDto);
         return ResponseEntity.ok(response);
     }
