6855f1d589c35172a64df67f9e25d8f56269a96e
paolap <paopin2024@gmail.com>
2025-09-04 19:31:16 +0200

    feat(challenge): complete CRUD operations and add admin-only security

--- src/main/java/com/reto/soyyo/controllers/ChallengeController.java
+++ src/main/java/com/reto/soyyo/controllers/ChallengeController.java
@@ -6,6 +6,7 @@ import com.reto.soyyo.services.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +18,9 @@ public class ChallengeController {
 
     private final ChallengeService challengeService;
 
+
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ChallengeResponse> createChallenge(@RequestBody ChallengeRequest request) {
         return new ResponseEntity<>(challengeService.createChallenge(request), HttpStatus.CREATED);
     }
@@ -33,12 +36,13 @@ public class ChallengeController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ChallengeResponse> updateChallenge(@PathVariable Long id,
-                                                             @RequestBody ChallengeRequest request) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ChallengeResponse> updateChallenge(@PathVariable Long id,@RequestBody ChallengeRequest request) {
         return new ResponseEntity<>(challengeService.updateChallenge(id, request), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteChallenge(@PathVariable Long id) {
         challengeService.deleteChallenge(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
